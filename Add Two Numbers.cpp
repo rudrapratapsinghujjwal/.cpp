dda25d4100f531bc6f30bb175e85cda572f1bcdf
Tarushi Singh <54908951+tarushisingh@users.noreply.github.com>
2020-10-01 20:16:42 +0530

    Updated this with some changes

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -1,21 +1,25 @@
-//sum of Two Numbers
-
-
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
-    
-    cout << "Enter two integers: ";
-    cin >> firstNumber >> secondNumber;
+   //Declaring two integer variables
+   
+    int num1, num2;
+   
+    /* cout displays the string provided in the
+    * double quotes as it is on the screen
+    */
+   
+    cout<<"Enter first integer number: ";
+   
+    /* cin is used to capture the user input
+    * and assign it to the variable.
+    */
+   
+    cin>>num1;
 
-    // sum of two numbers in stored in variable sumOfTwoNumbers
-    sumOfTwoNumbers = firstNumber + secondNumber;
-
-    // Prints sum 
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-
-    return 0;
+   cout<<"Enter second integer number: ";
+   cin>>num2;
+   cout<<"Sum of entered numbers is: "<<(num1+num2);
+   return 0;
 }
