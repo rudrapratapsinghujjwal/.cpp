430626cfde8918dcf79074b99fb861dac407f135
RudraPratap Singh Ujjwal <72209874+rudrapratapsinghujjwal@users.noreply.github.com>
2023-10-01 19:32:02 +0530

    Update  Find LCM using HCF.cpp

--- Find LCM using HCF.cpp
+++ Find LCM using HCF.cpp
@@ -1,19 +1,13 @@
-/// Find LCM using HCF
-
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
     int n1, n2, hcf, temp, lcm;
-
     cout << "Enter two numbers: ";
     cin >> n1 >> n2;
-
     hcf = n1;
     temp = n2;
-    
     while(hcf != temp)
     {
         if(hcf > temp)
